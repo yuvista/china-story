f32e1bb99bbd17478f2e423504cdddfc4b5dec00
Tom <hzlhu.dargon@gmail.com>
2012-11-25 20:55:54 +0800

    初始化代码库

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChinaStory::Application.config.secret_token = 'd81be3ae104fc4ab09f15eb1a6590b2b5d4fb9f0163d0ffca57b675eb3027d92394bc7fe82fdb09e10fe0f2aa6a19fa4939746583fa3308b66249f3c7da90647'
