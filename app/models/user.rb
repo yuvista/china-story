ffd804da1f40d20956e6eef868ac356a9801783d
Tom <hzlhu.dargon@gmail.com>
2012-12-07 11:44:27 +0800

    complete user signin

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User
+  include Mongoid::Document
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :encryptable
+
+  ## Database authenticatable
+  field :user_name,  :type => String, :default => ""
+  field :email,              :type => String, :default => ""
+  field :encrypted_password, :type => String, :default => ""
+
+  validates_presence_of :email
+  validates_presence_of :encrypted_password
+
+  ## Recoverable
+  field :reset_password_token,   :type => String
+  field :reset_password_sent_at, :type => Time
+
+  ## Rememberable
+  field :remember_created_at, :type => Time
+
+  ## Trackable
+  field :sign_in_count,      :type => Integer, :default => 0
+  field :current_sign_in_at, :type => Time
+  field :last_sign_in_at,    :type => Time
+  field :current_sign_in_ip, :type => String
+  field :last_sign_in_ip,    :type => String
+
+  field :password_salt, :type => String
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ChinaStory::Application.routes.draw do
+  devise_for :users
+
   root :to => 'home#index'
 
   match ':controller(/:action(/:id))(.:format)'
