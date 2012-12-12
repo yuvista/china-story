9cb97c41325e5c40739a1fd2e6b3df2573d679c7
Tom <hzlhu.dargon@gmail.com>
2012-12-12 17:48:28 +0800

    complete eras list

---
+++ app/helpers/eras_helper.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+module ErasHelper
+  def eras_tbody
+    eras = YAML.load_file 'config/eras_list.yml'
+
+    eras.map do |era|
+      style_class = 'center'
+      if era['belongs'].blank?
+        style_class = ''
+      end
+
+      html =<<-HTML
+        <tr>
+          <td class="#{style_class}">#{era['title']}</td>
+          <td>#{era['range']}</td>
+          <td>#{era['extra']}</td>
+        </tr>
+      HTML
+    end.join.html_safe
+  end
+end
