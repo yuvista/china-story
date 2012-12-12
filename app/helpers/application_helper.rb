664ac23cac0bb3e1be541ec1d76ac5f3ed216955
Tom <hzlhu.dargon@gmail.com>
2012-12-13 04:18:55 +0800

    重构：抽取可重用的逻辑

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,18 +34,30 @@ module ApplicationHelper
   def user_menu
     user_info = link_to '个人资料', edit_user_registration_path
     sign_out = link_to '退出', destroy_user_session_path
+    menu current_user.user_name, 'user_menu', [user_info, sign_out]
+  end
+
+  def menu menu_name, menu_id, menu_items
+    menu_size = menu_items.size
+    menu_items = menu_items.map do |menu_item|
+      style_class = ''
+      index = menu_items.index menu_item
+      if index == 0
+        style_class = 'first'
+      elsif index == menu_size -1
+        style_class = 'last'
+      end
+      content_tag :li, menu_item, :class => style_class
+    end.join
 
     html = <<-HTML
-      <li class="dropdown" id="user_menu">
-        <a href="#user_menu" class="dropdown-toggle" data-toggle= "dropdown">
-          #{current_user.user_name}
+      <li class="dropdown" id="#{menu_id}">
+        <a href="#{'#' + menu_id}" class="dropdown-toggle" data-toggle="dropdown">
+          #{menu_name}
           <b class="caret"></b>
         </a>
-        <ul class="dropdown-menu">
-          <li class="first">#{user_info}</li>
-          <li class="last">#{sign_out}</li>
-        </ul>
-      <li>
+        <ul class="dropdown-menu">#{menu_items}</ul>
+      </li>
     HTML
 
     html.html_safe
